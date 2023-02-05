65174572cbb2b6b29ef7bde795559ed71bbf2571
wallacejoe <JAWcoder@gmail.com>
2023-02-05 13:58:54 -0700

    commit after adding gitignore

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -19,23 +19,3 @@ x.onclick = toggleMenu;
 if (now.getDay() < 3 && now.getDay() > 0){
     document.getElementById("meetAndGreet").classList.toggle("activeBanner");
 }
-
-/*Current Weather and Temperature*/
-const url = "http://api.openweathermap.org/geo/1.0/direct?q=Mesa,85202,US&limit=&appid=61c80ef5042813383d91c99a65c305a0";
-
-async function apiFetch() {
-    try {
-      const response = await fetch(url);
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data); // this is for testing the call
-        // displayResults(data);
-      } else {
-          throw Error(await response.text());
-      }
-    } catch (error) {
-        console.log(error);
-    }
-  }
-  
-apiFetch();
