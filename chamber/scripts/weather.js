bc237137956d29459c5f2dda3c2e11dce182d107
wallacejoe <JAWcoder@gmail.com>
2023-02-06 01:48:10 -0700

    Added weather styles, HTML and javascript.

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -2,30 +2,52 @@
 const url = "https://api.openweathermap.org/geo/1.0/direct?q=Mesa,US&appid=61c80ef5042813383d91c99a65c305a0";
 
 async function apiFetch() {
-    try {
-      const response = await fetch(url);
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data); // this is for testing the call
-        // displayResults(data);
-      } else {
-          throw Error(await response.text());
-      }
-    } catch (error) {
-        console.log(error);
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      // displayResults(data);
+      let lon = data[0]["lon"];
+      let lat = data[0]["lat"];
+      weatherFetch(lat, lon);
+    } else {
+        throw Error(await response.text());
     }
+  } catch (error) {
+      console.log(error);
   }
-
-//Code found on StackOverflow.
-async function weatherBalloon(){
-  fetch("https://api.openweathermap.org/geo/1.0/direct?q=Mesa,US&appid=61c80ef5042813383d91c99a65c305a0")
-  .then(function(resp) {return resp.json()})
-  .then(function(data) {console.log(data);})
-  .catch(function() {console.log("Error with the weatherBalloon function")})
 }
 
-window.onload = function(){
-  weatherBalloon(6167865);
+async function weatherFetch(lat, lon){
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=61c80ef5042813383d91c99a65c305a0`;
+  try {
+    const response2 = await fetch(weatherUrl);
+    if (response2.ok){
+      const weatherData = await response2.json();
+      console.log(weatherData);
+      const windTemp = weatherData.main.temp;
+      document.getElementById("temperature").innerHTML = parseFloat(windTemp).toFixed(0);
+      const windSpeed = weatherData.wind.speed;
+      document.getElementById("windSpeed").innerHTML = parseFloat(windSpeed).toFixed(1);
+      windchillCalculation(windTemp, windSpeed);
+      const condition = weatherData.weather[0].description;
+      document.getElementById("condition").innerHTML = condition;
+      const weatherIcon = weatherData.weather[0].icon;
+      document.getElementById("weatherIcon").setAttribute("src", `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function windchillCalculation(t, v){
+  const windChill = 35.74 + (0.6215 * t) - (35.75 * (v ** 0.16)) + ((0.4275 * t) * (v ** 0.16))
+  let stringWindChill = windChill.toString();
+  if (stringWindChill == "NaN"){
+    stringWindChill = "N/A";
+  }
+  document.getElementById("windChill").innerHTML = parseFloat(stringWindChill).toFixed(0);
 }
 
 apiFetch();
