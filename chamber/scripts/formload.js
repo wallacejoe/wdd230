dffad1b09a8b45841ebb47bdc9ddb6c4688439b7
wallacejoe <JAWcoder@gmail.com>
2023-03-08 16:46:54 -0700

    Form load time compressed to single element.

--- chamber/scripts/formload.js
+++ chamber/scripts/formload.js
@@ -1,8 +1,13 @@
-//Current Date Join Form
+/*//Current Date Join Form
 let formLoadDate = document.querySelector(".formLoadDate");
 formLoadDate.textContent = fulldate;
 
 //Current Time Join Form
 let formLoadTime = document.querySelector(".formLoadTime");
 const fulltime = `${now.getHours()}:${now.getMinutes()}.${now.getSeconds()}`;
-formLoadTime.textContent = fulltime;
+formLoadTime.textContent = fulltime;*/
+
+//Get current date and time
+let formLoadTime = document.querySelector(".formLoadTime");
+const fulltime = `${now.getHours()}:${now.getMinutes()}.${now.getSeconds()}`;
+formLoadTime.textContent = `${fulldate} at ${fulltime}`;
